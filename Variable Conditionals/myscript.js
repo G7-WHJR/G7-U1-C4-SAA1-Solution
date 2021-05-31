a206134409392b842d1d972f7cbf489ec7448f57
G7-WHJR <raveen.anand@whitehatjr.com>
2021-05-31 22:11:32 +0530

    First

---
+++ Variable Conditionals/myscript.js
@@ -0,0 +1,19 @@
+function displayResult()
+{
+    // 1. Take input from input box and store it in a variable named "moneySaved"
+    var moneySaved= document.getElementById('enteredValue').value;
+
+    // 2. Check :
+        //if moneySaved is greater than 100 then 
+                //print a message "Great Job! You saved alot of money!" 
+        //else 
+                //print "You can do better! Try Saving more money next time!"
+
+   if(moneySaved > 50){
+           console.log("Great Job! You saved alot of money!");
+   }
+   else{
+        console.log("You can do better! Try Saving more money next time!");
+   }
+}
+
